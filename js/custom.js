140e596b32a8d5b47abc5b4f98b5b75e3990c1a0
Saaddevrim <83429492+Saaddevrim@users.noreply.github.com>
2021-06-21 01:41:54 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$('.banner_main').slick({
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    speed:1500,
+    dots: true,
+    infinite: true,
+  });
+
+  $(document).ready(function(){
+    $('.venobox').venobox(); 
+});
